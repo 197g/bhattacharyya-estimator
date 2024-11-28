b97829045c902a9220f8655f285d9bae1561eb9e
Andreas Molzer <andreas.molzer@gmx.de>
2024-11-28 22:33:19 +0100

    Implement confidence levels for BC estimate

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,135 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use statrs::distribution::ContinuousCDF;
+
+#[derive(Clone, Debug)]
+pub struct Estimate {
+    /// An estimate of the Bhattacharyya coefficient.
+    ///
+    /// On average this is an over estimation. In the limit of samples it is the true coefficient.
+    pub bc_estimate: f64,
+    pub hc_squared: f64,
+    pub total_variance_upper: f64,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct ConfidenceLevel {
+    dkw_constant: f64,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ConfidenceLevel {
+    /// Special confidence level that does not provide any confidence.
+    pub const NONE: Self = ConfidenceLevel { dkw_constant: 0.0 };
+
+    /// Pre-computed confidence level with less than 5% error.
+    pub const P95: Self = ConfidenceLevel {
+        dkw_constant: 1.3581015157406195,
+    };
+
+    /// Pre-computed confidence level with less than 2% error.
+    pub const P98: Self = ConfidenceLevel {
+        dkw_constant: 1.5174271293851462,
+    };
+
+    /// Pre-computed confidence level with less than 1% error.
+    pub const P99: Self = ConfidenceLevel {
+        dkw_constant: 1.6276236307187293,
+    };
+
+    pub fn new(level: f64) -> Self {
+        assert!(level > 0.0);
+        assert!(level < 1.0);
+
+        ConfidenceLevel {
+            dkw_constant: Self::dvoretzky_kiefer_wolfowitz_constant(level),
+        }
+    }
+
+    pub fn apply(&self, sorted: &[f64], cdf: &dyn ContinuousCDF<f64, f64>) -> Estimate {
+        assert!(!sorted.is_empty(), "No estimate for empty sample");
+
+        let count = sorted.len() as f64;
+        let sqrt_n = count.sqrt();
+
+        let skip = count.powf(2.0 / 3.0).ceil() as usize;
+
+        // Quantiles according to limit distribution.
+        let ps = (0..sorted.len())
+            .step_by(skip)
+            .map(|n| (1. + n as f64) / count);
+
+        // Quantiles according to model CDF.
+        let qs = sorted.chunks(skip).map(|arr| cdf.cdf(arr[0]));
+
+        let expand = self.dkw_constant / sqrt_n;
+        Estimate::from_matched_quantiles(ps.collect(), qs.collect(), expand)
+    }
+
+    /// Adjustment of CDF with Dvoretzky-Kiefer-Wolfowitz bounds:
+    ///
+    /// P(sup|F_n - F| > eps) <= 2 * exp(-2n * eps**2)
+    ///
+    /// We choose the confidence level `P(..) <= 2 * exp(-2n * eps**2) <= p`.
+    ///
+    /// ```text
+    /// 1 >= (2/p) * exp(-2n * eps**2)
+    /// 0 >= log(2/p) + (-2n * eps**2)
+    /// eps**2 >= log(2/p) / (2n)
+    /// eps >= sqrt(log(2 / p) / 2) / sqrt(n)
+    /// ```
+    ///
+    /// This only returns the constant which does not depend on `n`.
+    fn dvoretzky_kiefer_wolfowitz_constant(level: f64) -> f64 {
+        assert!(level < 1.0);
+        ((2.0f64.ln() - level.ln()) / 2.0).sqrt()
+    }
+}
+
+impl Estimate {
+    pub fn from_ecdf(sorted: &[f64], cdf: &dyn ContinuousCDF<f64, f64>) -> Self {
+        assert!(!sorted.is_empty(), "No estimate for empty sample");
+
+        let count = sorted.len() as f64;
+        let skip = count.sqrt().ceil() as usize;
+
+        // Quantiles according to limit distribution.
+        let ps = (0..sorted.len())
+            .step_by(skip)
+            .map(|n| (1. + n as f64) / count);
+
+        // Quantiles according to model CDF.
+        let qs = sorted.chunks(skip).map(|arr| cdf.cdf(arr[0]));
+
+        Self::from_matched_quantiles(ps.collect(), qs.collect(), 0.0)
+    }
+
+    fn from_matched_quantiles(mut ps: Vec<f64>, mut qs: Vec<f64>, expand: f64) -> Self {
+        Self::diff_in_place_with_added_bias(&mut ps, 0.0);
+        Self::diff_in_place_with_added_bias(&mut qs, expand);
+
+        let p_weight = ps.iter().copied();
+        let q_weight = qs.iter().copied();
+
+        // Over-estimation of the real, plus bias.
+        let bc_estimate: f64 = p_weight
+            .zip(q_weight)
+            .map(|(lp, lq)| (lp * lq).sqrt())
+            .sum();
+
+        // Under-estimation of H²
+        let hc_squared = 1.0 - bc_estimate;
+        // High Bound of TVD based on fundamental form of Hellinger distance.
+        let total_variance_upper = 2.0f64.sqrt() * hc_squared.sqrt();
+
+        Estimate {
+            bc_estimate,
+            hc_squared,
+            total_variance_upper,
+        }
+    }
+
+    fn diff_in_place_with_added_bias(slice: &mut [f64], expand: f64) {
+        let mut state = 0.0;
+        slice.iter_mut().for_each(|x| {
+            let pre = core::mem::replace(&mut state, *x);
+            *x = (*x + expand).clamp(0.0, 1.0) - (pre - expand).clamp(0.0, 1.0);
+        })
     }
 }
--- tests/distribution-std.rs
+++ tests/distribution-std.rs
@@ -1,14 +1,59 @@
+use estimated_hellinger::{ConfidenceLevel, Estimate};
+
 use rand::{distributions::OpenClosed01, Rng as _};
-use statrs::distribution::{ContinuousCDF as _, Normal};
+use statrs::distribution::{ContinuousCDF, Normal};
 
 #[test]
 fn generate_sample() {
     let n = Normal::new(0.0, 1.0).unwrap();
     const COUNT: usize = 1_000_000;
 
-    let v: Vec<f64> = rand::thread_rng()
+    let mut v: Vec<f64> = rand::thread_rng()
         .sample_iter(OpenClosed01)
         .take(COUNT)
         .map(|x| n.inverse_cdf(x))
         .collect();
+
+    v.sort_by(|a, b| a.total_cmp(b));
+
+    // Against itself, we have a very very high BC.
+    check_ecdf(&v, &n, 0.99);
+
+    let above = Normal::new(0.5, 1.0).unwrap();
+
+    let bc = optimal_square_hellinger((0.0, 1.0), (0.5, 1.0));
+    check_ecdf(&v, &above, bc);
+}
+
+fn check_ecdf(v: &[f64], n: &dyn ContinuousCDF<f64, f64>, min_bc: f64) {
+    // The bad confidence levels, and in particular none-confidence level, will happen to
+    // underestimate the true one by a tiny bit sometimes. That's expected?
+    let min_bc = 0.98 * min_bc;
+
+    let max_hc = 1.0 - min_bc;
+
+    let estimate = Estimate::from_ecdf(v, n);
+    assert!(estimate.bc_estimate >= min_bc, "{estimate:?}");
+    assert!(estimate.hc_squared < max_hc, "{estimate:?}");
+
+    let e95 = ConfidenceLevel::P95.apply(v, n);
+    assert!(e95.bc_estimate >= min_bc, "{estimate:?}");
+    assert!(e95.hc_squared < max_hc, "{estimate:?}");
+
+    let e99 = ConfidenceLevel::P99.apply(v, n);
+    assert!(e99.bc_estimate >= min_bc, "{estimate:?}");
+    assert!(e99.hc_squared < max_hc, "{estimate:?}");
+
+    assert!(estimate.bc_estimate <= e95.bc_estimate);
+    assert!(e95.bc_estimate <= e99.bc_estimate);
+}
+
+fn optimal_square_hellinger(a: (f64, f64), b: (f64, f64)) -> f64 {
+    let (m1, s1) = a;
+    let (m2, s2) = b;
+
+    let v1 = s1.sqrt();
+    let v2 = s2.sqrt();
+
+    ((2.0 * v1 * v2) / (s1 + s2)).sqrt() * (-0.25 * (m1 - m2).powi(2) / (s1 + s2)).exp()
 }
